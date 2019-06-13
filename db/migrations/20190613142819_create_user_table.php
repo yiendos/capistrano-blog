0fcc838bfeb0dbcea3898155642434e2866383bc
yiendos <yiendos@sbmatrix.com>
2019-06-13 15:33:49 +0100

    #master drop table and create users

---
+++ db/migrations/20190613142819_create_user_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateUserTable extends AbstractMigration
+{
+    public function up()
+    {
+
+        $sql = <<<EOT
+        
+DROP TABLE IF EXISTS `j_users` ; 
+        
+CREATE TABLE `j_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(400) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `username` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `block` tinyint(4) NOT NULL DEFAULT 0,
+  `sendEmail` tinyint(4) DEFAULT 0,
+  `registerDate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `lastvisitDate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `activation` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `params` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastResetTime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Date of last password reset',
+  `resetCount` int(11) NOT NULL DEFAULT 0 COMMENT 'Count of password resets since lastResetTime',
+  `otpKey` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'Two factor authentication encrypted keys',
+  `otep` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'One time emergency passwords',
+  `requireReset` tinyint(4) NOT NULL DEFAULT 0 COMMENT 'Require user to reset password on next login',
+  PRIMARY KEY (`id`),
+  KEY `idx_name` (`name`(100)),
+  KEY `idx_block` (`block`),
+  KEY `username` (`username`),
+  KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=954 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+EOT;
+
+        $this->execute($sql);
+
+    }
+    public function down()
+    {
+
+        $sql = <<<EOT
+
+DROP TABLE `j_users`;
+
+EOT;
+
+        $this->execute($sql);
+
+    }
+}
