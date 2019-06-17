2e20cc1566713a7087a615a8c989424cad0e2144
yiendos <yiendos@sbmatrix.com>
2019-06-17 14:24:40 +0100

    #staging: be sure to provide user notes in migration

--- db/migrations/20190617123805_add_user_notes.php
+++ db/migrations/20190617123805_add_user_notes.php
@@ -10,6 +10,12 @@ class AddUserNotes extends AbstractMigration
         $sql = <<<EOT
         
 ALTER TABLE `j_users` ADD `notes` TEXT DEFAULT NULL AFTER `password`;
+
+UPDATE `j_users` SET `notes` = 'Likes coffee' WHERE `id` = '951'; 
+
+UPDATE `j_users` SET `notes` = 'Likes tea' WHERE `id` = '952'; 
+
+UPDATE `j_users` SET `notes` = 'Likes chai' WHERE `id` = '953';
 EOT;
 
         $this->execute($sql);
